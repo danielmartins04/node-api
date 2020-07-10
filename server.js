b8a74c33d6abda3369aa35785ce0129f1cd409a8
daniel martins <freitasdaniel39@gmail.com>
2020-07-10 13:27:31 -0300

    restructuring of files

--- server.js
+++ server.js
@@ -8,17 +8,8 @@ mongoose.connect('mongodb://localhost/nodeapi', { useNewUrlParser: true });
 
 require('./src/models/Product');
 
-const Product = mongoose.model('Product');
-
-app.get('/', (req, res) => {
-    Product.create({ 
-        title: 'React Native',
-        description: 'Build native apps with React',
-        url: 'url'
-    });
-
-    res.send("<h1>asas waoarldasasa</h1>");
-});
+// Rotas
+app.use('/', require("./src/routes"));
 
 app.listen(3001, () => {
     console.log("Servidor rodando na porta 3001");
---
+++ src/controllers/ProductController.js
@@ -0,0 +1,11 @@
+const mongooose = require("mongoose");
+
+const Product = mongooose.model('Product');
+
+module.exports = {
+    async index(req, res) {
+        const products = await Product.find();
+
+        return res.json(products);
+    },
+}
