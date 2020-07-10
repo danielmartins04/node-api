18b5d5bb6747dea6492e767e91040bdd0af777b1
daniel martins <freitasdaniel39@gmail.com>
2020-07-10 11:04:20 -0300

    creating product model

--- server.js
+++ server.js
@@ -6,7 +6,17 @@ const mongoose = require("mongoose");
 // Iniciando o DB
 mongoose.connect('mongodb://localhost/nodeapi', { useNewUrlParser: true });
 
+require('./src/models/Product');
+
+const Product = mongoose.model('Product');
+
 app.get('/', (req, res) => {
+    Product.create({ 
+        title: 'React Native',
+        description: 'Build native apps with React',
+        url: 'url'
+    });
+
     res.send("<h1>asas waoarldasasa</h1>");
 });
 
