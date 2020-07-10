13877b22b5af620daafbc4655155c31b8d3da71a
daniel martins <freitasdaniel39@gmail.com>
2020-07-10 10:52:59 -0300

    connect to database

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 const express = require("express");
 const app = express();
 
+const mongoose = require("mongoose");
+
+// Iniciando o DB
+mongoose.connect('mongodb://localhost/nodeapi', { useNewUrlParser: true });
+
+app.get('/', (req, res) => {
+    res.send("<h1>asas waoarldasasa</h1>");
+});
+
 app.listen(3001, () => {
     console.log("Servidor rodando na porta 3001");
-});
+}); 
